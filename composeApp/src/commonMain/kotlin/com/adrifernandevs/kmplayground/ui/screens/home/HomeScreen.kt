7a324b1bafb5e3b18d21f7a964ccf043da80b8f3
adriafernandez <adrifernandevs@gmail.com>
2024-08-12 11:02:57 +0200

    Favorite feature implemented on HomeScreen

--- composeApp/src/commonMain/kotlin/com/adrifernandevs/kmplayground/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/adrifernandevs/kmplayground/ui/screens/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.adrifernandevs.kmplayground.ui.screens.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -11,7 +12,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
@@ -32,6 +37,7 @@ import com.adrifernandevs.kmplayground.ui.screens.Screen
 import com.adrifernandevs.kmplayground.ui.screens.home.viewmodel.HomeViewModel
 import kmplayground.composeapp.generated.resources.Res
 import kmplayground.composeapp.generated.resources.app_name
+import kmplayground.composeapp.generated.resources.favorite
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
@@ -89,15 +95,29 @@ private fun MovieItem(
             .clickable { onMovieClicked(movie) },
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AsyncImage(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(2 / 3f)
-                .clip(MaterialTheme.shapes.medium),
-            model = movie.poster,
-            contentDescription = movie.title,
-            contentScale = ContentScale.Crop
-        )
+        Box {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(2 / 3f)
+                    .clip(MaterialTheme.shapes.medium),
+                model = movie.poster,
+                contentDescription = movie.title,
+                contentScale = ContentScale.Crop
+            )
+
+            if (movie.isFavorite) {
+                Icon(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.TopEnd),
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = stringResource(Res.string.favorite),
+                    tint = Color.White
+                )
+            }
+        }
+
         Text(
             modifier = Modifier.fillMaxWidth(),
             text = movie.title,
