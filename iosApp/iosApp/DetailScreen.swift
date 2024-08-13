a98e0e8a0874413ce98d9e45f01f1494d6663325
adriafernandez <adrifernandevs@gmail.com>
2024-08-13 10:01:31 +0200

    Implementing view for DetailScreen

---
+++ iosApp/iosApp/DetailScreen.swift
@@ -0,0 +1,83 @@
+//
+//  DetailScreen.swift
+//  iosApp
+//
+//  Created by Adrià Fernández Arans on 13/8/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+import ComposeApp
+
+struct DetailScreen: View {
+    var viewModel: DetailViewModel
+    
+    init(movieId: Int32){
+        viewModel = DetailViewModel(movieId: movieId)
+    }
+    
+    var body: some View {
+        Observing(viewModel.state) { state in
+            VStack {
+                if(state.isLoading) {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                }
+                
+                if let movieDetail = state.movie {
+                    MovieDetail(
+                        movie: movieDetail,
+                        onFavoriteClick: { viewModel.onEvent(event: DetailViewModel.UiEventOnFavouriteClicked()) }
+                    )
+                }
+            }
+        }
+    }
+}
+
+struct MovieDetail: View {
+    var movie: Movie
+    var onFavoriteClick: () -> Void
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                if let backdrop = movie.backdrop {
+                    AsyncImage(url: URL(string: backdrop)) { image in
+                        image
+                            .resizable()
+                            .frame(maxWidth: .infinity)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .aspectRatio(16 / 9, contentMode: .fill)
+                    .frame(maxHeight: 200)
+                    .clipped()
+                }
+                Text(movie.overview)
+                    .padding()
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("**Original language**: \(movie.originalLanguage)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("**Original title**: \(movie.originalTitle)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("**Release date**: \(movie.releaseDate)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("**Popularity**: \(movie.popularity)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("**Vote average**: \(movie.voteAverage)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .padding()
+                .background(Color.secondary.opacity(0.1))
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                .cornerRadius(8)
+            }
+        }
+        .navigationTitle(movie.title)
+        .navigationBarItems(trailing: Button(action: onFavoriteClick) {
+            Image(systemName: movie.isFavorite ? "heart.fill" : "heart")
+        })
+    }
+}
--- iosApp/iosApp/HomeScreen.swift
+++ iosApp/iosApp/HomeScreen.swift
@@ -15,24 +15,86 @@ struct HomeScreen: View {
     var viewModel = HomeViewModel()
     
     var body: some View {
-        Observing(viewModel.state){ state in
-            if (state.isLoading) {
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle())
-            }
-            if (!state.movies.isEmpty) {
-                let columns = [GridItem(.adaptive(minimum: 100))]
-                
-                ScrollView{
-                    LazyVGrid(columns: columns){
-                        ForEach(state.movies) { movie in
-                            Text(movie.title)
+        NavigationView{
+            VStack {
+                Observing(viewModel.state){ state in
+                    if (state.isLoading) {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle())
+                    }
+                    if (!state.movies.isEmpty) {
+                        let columns = [
+                            GridItem(.flexible()),
+                            GridItem(.flexible())
+                        ]
+                        
+                        ScrollView{
+                            LazyVGrid(columns: columns){
+                                ForEach(state.movies) { movie in
+                                    NavigationLink(destination:
+                                        DetailScreen(movieId: movie.id)
+                                    ){
+                                        MovieItemView(movie: movie)
+                                    }
+                                }
+                            }.padding()
                         }
                     }
                 }
             }
+            .navigationTitle(Text("KMPlayground"))
         }.onAppear {
             viewModel.onEvent(event: HomeViewModel.UiEventOnUiReady())
+        }
+        
+    }
+}
+
+struct MovieItemView: View {
+    var movie: Movie
+    
+    var body: some View {
+        VStack {
+            GeometryReader { geometry in
+                ZStack(alignment: .topTrailing){
+                    AsyncImage(url: URL(string: movie.poster)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle())
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(2 / 3, contentMode: .fill)
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                                .clipped()
+                                .cornerRadius(8)
+                        case .failure:
+                            Image(systemName: "photo")
+                                .resizable()
+                                .aspectRatio(2 / 3, contentMode: .fill)
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                                .clipped()
+                                .cornerRadius(8)
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                }
+                if (movie.isFavorite) {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.white)
+                        .padding(5)
+                }
+            }
+            .aspectRatio(2/3, contentMode: .fit)
+            
+            Text(movie.title)
+                .font(.caption)
+                .tint(Color.black)
+                .lineLimit(1)
+                .padding(5)
         }
     }
 }
